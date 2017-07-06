4bfca18c210b89636b60e2cc59b63c3661225a36
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-07-05 21:50:02 -0400

    Removed stub code from main

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -9,27 +9,5 @@
 #include <iostream>
 
 int main() {
-	glfwInit();
-
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-	std::cout << extensionCount << " extensions supported" << std::endl;
-
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-	}
-
-	glfwDestroyWindow(window);
-
-	glfwTerminate();
-
 	return 0;
 }
