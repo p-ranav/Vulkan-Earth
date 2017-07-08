e6fb67bda93d765958f579bc3871f49c82523bc6
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-07-07 20:07:37 -0400

    Able to render a rotating icosphere!

---
+++ Engine/Sphere.h
@@ -0,0 +1,54 @@
+#pragma once
+
+// User-defined Headers
+#include "Vertex.h"
+
+// System Headers
+#include <utility>
+#include <vector>
+
+// Define PI for local use
+#define M_PI 3.1415926535897932384626433832795
+#define M_2PI M_PI * 2
+
+namespace Engine {
+
+	std::pair<std::vector<Vertex>, std::vector<uint16_t>> CreateSphere(float radius, float slices, float stacks) {
+
+		std::vector<Vertex> vertices;
+		std::vector<uint16_t> indices;
+
+		for (int i = 0; i <= stacks; ++i) {
+
+			// V texture coordinate
+			double V = i / static_cast<double>(stacks);
+			double phi = V * M_PI;
+
+			for (int j = 0; j <= slices; ++j)
+			{
+				// U texture coordinate
+				double U = j / static_cast<double>(slices);
+				double theta = U * M_2PI;
+
+				double X = cos(theta) * sin(phi);
+				double Y = cos(phi);
+				double Z = sin(theta) * sin(phi);
+
+				// Add this vertex (with white color)
+				vertices.push_back({ {X * radius, Y * radius, Z * radius }, {1, 1, 1} });
+			}
+		}
+
+		for (int i = 0; i < slices * stacks + slices; ++i) {
+			indices.push_back(i);
+			indices.push_back(i + slices + 1);
+			indices.push_back(i + slices);
+
+			indices.push_back(i + slices + 1);
+			indices.push_back(i);
+			indices.push_back(i + 1);
+		}
+
+		return std::make_pair(vertices, indices);
+	}
+}
